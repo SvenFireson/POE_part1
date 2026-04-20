07f2f969cf30cd1399ae47573e7c20792e9bd25e
SvenFireson <sven.fireson@gmail.com>
2026-04-20 14:42:37 -0400

    created attributes

--- src/main/java/com/mycompany/poe_part1/Login.java
+++ src/main/java/com/mycompany/poe_part1/Login.java
@@ -1,8 +1,42 @@
 
 package com.mycompany.poe_part1;
 
-
+//These are all my attributes to store user information for login
 public class Login {
+    private String firtName;
+    private String lastName;
+    private String password;
+    private String username;
+    private String cellPhoneNumber;
     
     
+    // this ensures that I am allowing allocations for my attributtes
+public Login(String firstName, String lastName, String password, String username, String cellPhoneNumber){
+    this.firtName = firstName;
+    this.lastName = lastName;
+    this.password = password;
+    this.username = username;
+    this.cellPhoneNumber = cellPhoneNumber;
+
+}
+
+// creating methods for attribuites using booleans
+ public boolean  checkUserName(){
+ return false;} 
+ 
+ public boolean checkPasswordComplexity(){
+ return false;}
+ 
+ public boolean checkCellPhoneNumber(){
+ return false;}
+ 
+ public String registerUser(){
+ return "";}
+ 
+ public boolean loginUser(String inputUserName, String inputPassword){
+ return false;}
+ 
+ public String returnLoginStatus(boolean loginSuccess){
+ return "";}
+ 
 }
